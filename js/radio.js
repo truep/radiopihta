9231fec7a294123a2258f2226f0147d034338e9c
Matyukhin Danil <post7@duty-pc.is74.ru>
2014-08-30 13:39:21 +0600

    loading pages on click in menu without main page reload

--- js/radio.js
+++ js/radio.js
@@ -1,22 +1,33 @@
 /*http://radiopixta.zapto.org/script/track.txt*/
- function show()  
-        {  
-            $.ajax({  
-                url: "txt/track.txt",  
-                cache: false,  
-                success: function(html){  
-                    $("#title").html(html);  
-                }  
-            });  
-        }  
-      
-        $(document).ready(function(){  
-            show();  
-            setInterval('show()',5000);  
-        });  
+function show() {
+    $.ajax({
+        url: "txt/track.txt",
+        cache: false,
+        success: function (html) {
+            $("#title").html(html);
+        }
+    });
+}
 
+$(document).ready(function () {
+    show();
+    setInterval('show()', 5000);
+});
 
-setInterval(function(){
-$("#title").load("txt/track.txt");
+setInterval(function () {
+    $("#title").load("txt/track.txt");
 }, 5000);
 
+$(function () {
+    $('#loadcontent').load('content/main.html');
+
+    $('ul#menu li').click(function () {
+        var linkname = $(this).attr('class');
+        var link = 'content/' + linkname + '.html';
+        if (linkname != "download") {
+            $('#loadcontent').load(link);
+            $('ul#menu li').children('a').removeClass('active');
+            $(this).children('a').addClass('active');
+        } else {}
+    });
+});
